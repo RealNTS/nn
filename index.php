fd571386babeaab603b56ff1d2fde8b96f44bf48
RealNTS <106115430+RealNTS@users.noreply.github.com>
2022-06-13 20:43:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+	/*
+					INFO
+		------------------------------------
+		Tên: Nhân Light
+		Sinh năm: 2K5
+		FB: https://facebook.com/votrungnhanmmo
+
+		* Vui lòng không xóa dòng nầy để tôn trọng tác giả!
+	*/
+
+	//gọi route
+	require_once __DIR__."/app/include/core/route.php";
+?>
